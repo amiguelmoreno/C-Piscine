5f93c694efda758217788229c031ff6cc6f3a42c
Antonio Miguel Moreno Molina <migue@Antonios-MacBook-Pro.local>
2022-05-29 18:39:11 +0200

    fsdfasd

---
+++ 42-Cursus/ft_printf/ft_printf.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antmoren <antmoren@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 19:45:55 by antmoren          #+#    #+#             */
+/*   Updated: 2022/05/29 12:27:26 by antmoren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/ft_printf.h"
+
+int	ft_printf(char const *format, ...)
+{
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_printf("Hola\n");
+	printf("Hola\n");
+}
