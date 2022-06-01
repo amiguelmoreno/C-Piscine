bc4fc0e3c3db843939c15ef8dbef76e3d23e35c1
Antonio miguel Moreno molina <antmoren@c1r14s4.42malaga.com>
2022-06-01 14:36:56 +0200

    commit de todos los archivos

--- 42-Cursus/ft_printf/ft_printf.c
+++ 42-Cursus/ft_printf/ft_printf.c
@@ -6,22 +6,34 @@
 /*   By: antmoren <antmoren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 19:45:55 by antmoren          #+#    #+#             */
-/*   Updated: 2022/06/01 10:05:07 by antmoren         ###   ########.fr       */
+/*   Updated: 2022/06/01 14:36:31 by antmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/ft_printf.h"
 
-int	ft_conversion(char c, va_list ptr, int i)
+int	ft_conversion(char value, va_list ptr, int i)
 {
 	int		sub_cnt;
 	char	str;
 
 	sub_cnt = 0;
-	if (c == 'c')
-	{
-		ft_strlen(c);
-	}
+	if (value == 'c')
+		ft_treat_char(va_arg(ptr, int));
+	else if (value == 's')
+		ft_treat_string(va_arg(ptr, char *));
+	else if (value == 'p')
+		ft_treat_pointer(va_arg(ptr, void *));
+	else if (value == 'd' || value == 'i')
+		ft_treat_decimal(va_arg(ptr, ));
+	else if (value == 'u')
+		ft_treat_decimal(va_arg(ptr, ));
+	else if (value == 'x')
+		ft_treat_decimal(va_arg(ptr, ));
+	else if (value == 'X')
+		ft_treat_decimal(va_arg(ptr, ));
+	else if (value == '%')
+		ft_treat_decimal(va_arg(ptr, ));
 	return (sub_cnt);
 }
 
